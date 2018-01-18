a319fefd66764970809b645eb77efdadd6396e56
Ariel Ferdman <ariel-f@yit.co.il>
2018-01-18 14:58:16 +0200

    redist 0.4

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup, Extension
 strextnd_module = Extension('strextnd', sources = ['strextnd.c'])
 
 setup(name='strextnd',
-      version='0.3',
+      version='0.4',
       description='A Module written in C that extends the python3 str library',
       author = 'Ariel Ferdman',
       author_email = 'arielxcon@gmail.com',
       ext_modules=[strextnd_module],
       url = 'https://github.com/arielferdman/strextnd',
-      download_url = 'https://github.com/arielferdman/strextnd/archive/0.3.tar.gz',
+      download_url = 'https://github.com/arielferdman/strextnd/archive/0.4.tar.gz',
       keywords = ['string', 'is_numeric', 'float'],
       classifiers = [],)
