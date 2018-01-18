3df7ea94889028a96b392d871d939c0bdd5f2c42
Ariel Ferdman <ariel-f@yit.co.il>
2018-01-18 14:11:52 +0200

    new release to fix broken release uploaded to pypi

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup, Extension
 strextnd_module = Extension('strextnd', sources = ['strextnd.c'])
 
 setup(name='strextnd',
-      version='0.2',
+      version='0.3',
       description='A Module written in C that extends the python3 str library',
       author = 'Ariel Ferdman',
       author_email = 'arielxcon@gmail.com',
       ext_modules=[strextnd_module],
       url = 'https://github.com/arielferdman/strextnd',
-      download_url = 'https://github.com/arielferdman/strextnd/archive/0.2.tar.gz',
+      download_url = 'https://github.com/arielferdman/strextnd/archive/0.3.tar.gz',
       keywords = ['string', 'is_numeric', 'float'],
       classifiers = [],)
